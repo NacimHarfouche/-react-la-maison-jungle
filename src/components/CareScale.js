eac06a740c1516c34c11b69fd961a984f1842929
NacimHarfouche <nacim_4@hotmail.fr>
2022-08-04 18:38:25 +0200

    add footer and modified some file

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,6 +1,23 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
+function handleClickWater(value, type) {
+    switch (value) {
+        case 2:
+            value = "modérement"
+            break;
+        case 3:
+            value = "beaucoup"
+          break;
+        default:
+            value = "peu"
+    }
+    
+	type = type === 'light' ? "lumière" : "d'arrosage"
+
+    alert(`Cette plante requiert ${value} de ${type}`)
+}
+
 function CareScale({scaleValue, careType}) {
     const range = [1, 2, 3]
     const scaleType = 
@@ -9,7 +26,7 @@ function CareScale({scaleValue, careType}) {
             : <img src={Water} alt="water-icon" />
 
     return (
-        <div>
+        <div onClick={() => handleClickWater(scaleValue, careType)}>
             {range.map((rangeElem) =>
                 scaleValue >= rangeElem ? <span key={rangeElem.toString()}>{scaleType}</span> : null
             )}
--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,13 +1,26 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
+function handleClick(plantName, water, id, cover, light) {
+	console.log(plantName, water, id, cover, light)
+	alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+}
+
+function handleClickWater(value) {
+	console.log(value)
+}
+
+// function handleClickLight() {
+
+// }
+
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item'>
-			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+		<li key={id} className='lmj-plant-item' >
+			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} onClick={() => handleClick(name, water, id, cover, light)} />
 			{name}
 			<div>
-				<CareScale careType='water' scaleValue={water} />
+				<CareScale careType='water' scaleValue={water} onClick={() => handleClickWater(water)} />
 				<CareScale careType='light' scaleValue={light} />
 			</div>
 		</li>
